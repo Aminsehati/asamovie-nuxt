fd5acbcd03432d19dfe9a1a8f8cb82ef16dba47d
Amin Sehati <Aminsehati70@gmail.com>
2021-11-28 15:29:07 +0330

    ui page register

--- pages/actors/_id/index.vue
+++ pages/actors/_id/index.vue
@@ -1,33 +1,48 @@
 <template>
   <div class="actors_page">
     <div class="container-fluid">
-        <div class="title">
-            <div class="icon">
-                <img :src="avatarIcon"/>
-            </div>
-            <div class="content">
-                <h2>
-                    بهاره کیان افشار
-                </h2>
-            </div>
+      <div class="title">
+        <div class="icon">
+          <img :src="avatarIcon" />
         </div>
+        <div class="content">
+          <h2 class="font-16 font-400">بهاره کیان افشار</h2>
+          <Button
+            light
+            @onClick="filters.isFollowed = !filters.isFollowed"
+            :class="{ loading: filters.loadingButton }"
+          >
+            {{ filters.isFollowed ? "حذف کنید" : "دنبال کنید" }}
+          </Button>
+        </div>
+      </div>
+      <div class="actors_offer">
+        <h4 v-if="$auth.loggedIn">
+          yes
+        </h4>
+        <h4 v-else>
+          No
+        </h4>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-    import iconsMixin from '@/mixins/icons.js'
-    import './style.css'
+import iconsMixin from "@/mixins/icons.js";
+import "./style.scss";
 export default {
-    mixins:[iconsMixin],
-    data(){
-        return {
-
-        }
-    },
-}
+  mixins: [iconsMixin],
+  data() {
+    return {
+      filters: {
+        loadingButton: false,
+        isFollowed: true,
+      },
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ pages/player/_id.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="video__player">
+    <video
+      controls
+      id="myVideo"
+      src="https://hajifirouz5.cdn.asset.aparat.com/aparat-video/59f136ada1379566563526fbe40464cc39464485-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjQxZDc2ZjJhNzNhZGI2NmJiM2RjZjUwNzI3ZTI2NGQ4IiwiZXhwIjoxNjM3OTM0NjQzLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.Kg_HYZVgcYMrIP3TsnFl2j8JXeIEWog79yYzKXlwloE"
+      ref="video"
+    ></video>
+    <div id="controller">
+        sss
+        <div class="progress-bar">
+            <progress id="progress"  value="0" max="100">Progress</progress>
+        </div>
+        <div>
+            <div class="left">
+                <span @click="playVideo">
+                    <i class="fas fa-play"></i>
+                </span>
+                <span @click="pauseVideo">
+                    <i class="fas fa-pause"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./style.css";
+export default {
+  layout: "player",
+  methods:{
+    progressVideo(){
+
+    },
+    playVideo(){
+        this.$refs.video.play();
+    },
+    pauseVideo(){
+        this.$refs.video.pause();
+    }
+  },
+  mounted(){
+      this.progressVideo();
+  }
+};
+</script>
+
+<style>
+</style>
